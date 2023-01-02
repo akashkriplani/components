2743ced329d1cd1c0127d329002a6cdfb1b40d81
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-02 22:05:53 +0530

    Style link component using tailwind.css

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,8 +1,12 @@
 import { useContext } from 'react';
 import NavigationContext from '../context/navigation';
+import classnames from 'classnames';
 
 function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
+
+  const classes = classnames('text-blue-500');
+
   const handleClick = (event) => {
     // event.ctrlKey (windows) || event.metaKey (mac) returns true if the user does a control and click on the link to open a page in a new tab. In that case, we want the default browser navigation to occur using href attribute.
     if (event.ctrlKey || event.metaKey) {
@@ -14,7 +18,7 @@ function Link({ to, children }) {
   };
 
   return (
-    <a onClick={handleClick} href={to}>
+    <a className={classes} onClick={handleClick} href={to}>
       {children}
     </a>
   );
