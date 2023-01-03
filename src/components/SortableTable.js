24978416b2828b84f2639a9b764e0acd0b163296
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-03 21:44:39 +0530

    Add sorting icons

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { GoArrowSmallDown, GoArrowSmallUp } from 'react-icons/go';
 import Table from './Table';
 
 function SortableTable(props) {
@@ -27,9 +28,11 @@ function SortableTable(props) {
     return {
       ...column,
       header: () => (
-        <th onClick={() => handleClick(column.label)}>
-          {getIcons(column.label, sortBy, sortOrder)}
-          {column.label}
+        <th className="cursor-pointer hover:bg-gray-100" onClick={() => handleClick(column.label)}>
+          <div className="flex items-center">
+            {getIcons(column.label, sortBy, sortOrder)}
+            {column.label}
+          </div>
         </th>
       )
     };
@@ -67,15 +70,33 @@ function SortableTable(props) {
 
 function getIcons(label, sortBy, sortOrder) {
   if (label !== sortBy) {
-    return 'Show both icons';
+    return (
+      <div>
+        <GoArrowSmallUp />
+        <GoArrowSmallDown />
+      </div>
+    );
   }
 
   if (sortOrder === null) {
-    return 'Show both icons';
+    return (
+      <div>
+        <GoArrowSmallUp />
+        <GoArrowSmallDown />
+      </div>
+    );
   } else if (sortOrder === 'asc') {
-    return 'Show up icon';
+    return (
+      <div>
+        <GoArrowSmallUp />
+      </div>
+    );
   } else if (sortOrder === 'desc') {
-    return 'Show down icon';
+    return (
+      <div>
+        <GoArrowSmallDown />
+      </div>
+    );
   }
 }
 
