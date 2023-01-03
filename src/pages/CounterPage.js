15442ddc20b4ef4afe423b725e88e2eb0b8c38c9
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-04 01:15:51 +0530

    Completed counter page feature by adding action types

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -4,6 +4,8 @@ import Panel from '../components/Panel';
 
 const INCREMENT_COUNT = 'increment';
 const SET_VALUE_TO_ADD = 'change_value_to_add';
+const DECREMENT_COUNT = 'decrement';
+const ADD_VALUE_TO_COUNT = 'add_value_to_count';
 
 function reducer(state, action) {
   switch (action.type) {
@@ -16,6 +18,17 @@ function reducer(state, action) {
       return {
         ...state,
         valueToAdd: action.payload
+      };
+    case DECREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count - 1
+      };
+    case ADD_VALUE_TO_COUNT:
+      return {
+        ...state,
+        count: state.count + state.valueToAdd,
+        valueToAdd: 0
       };
     default:
       return state;
@@ -37,7 +50,9 @@ function CounterPage({ initialCount }) {
   };
 
   const decrement = () => {
-    // setCount(count - 1);
+    dispatch({
+      type: DECREMENT_COUNT
+    });
   };
 
   const handleChange = (event) => {
@@ -51,8 +66,10 @@ function CounterPage({ initialCount }) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // setCount(count + valueToAdd);
-    // setValueToAdd(0);
+
+    dispatch({
+      type: ADD_VALUE_TO_COUNT
+    });
   };
 
   return (
