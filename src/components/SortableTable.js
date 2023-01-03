33403a5842730d73cefd800dc07fe80ed5bac130
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-03 21:37:30 +0530

    Add a getIcons method to determine the icon set to use while sorting

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -26,7 +26,12 @@ function SortableTable(props) {
 
     return {
       ...column,
-      header: () => <th onClick={() => handleClick(column.label)}>{column.label} IS SORTABLE</th>
+      header: () => (
+        <th onClick={() => handleClick(column.label)}>
+          {getIcons(column.label, sortBy, sortOrder)}
+          {column.label}
+        </th>
+      )
     };
   });
 
@@ -58,6 +63,20 @@ function SortableTable(props) {
       <Table {...props} data={sortedData} config={updatedConfig} />
     </div>
   );
+}
+
+function getIcons(label, sortBy, sortOrder) {
+  if (label !== sortBy) {
+    return 'Show both icons';
+  }
+
+  if (sortOrder === null) {
+    return 'Show both icons';
+  } else if (sortOrder === 'asc') {
+    return 'Show up icon';
+  } else if (sortOrder === 'desc') {
+    return 'Show down icon';
+  }
 }
 
 export default SortableTable;
